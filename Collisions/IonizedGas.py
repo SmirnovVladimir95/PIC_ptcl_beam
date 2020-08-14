5d8324aab2ce5109bf620bc4b5942bb04da2fbcb
Roman-41 <romanzx301@gmail.com>
2020-08-14 19:43:07 +0300

    comment

--- Collisions/IonizedGas.py
+++ Collisions/IonizedGas.py
@@ -7,8 +7,7 @@ class IonizedGas(NeutralGas):
     def __init__(self, T, n, mass, Z, name=None):
         super(IonizedGas, self).__init__(T, n, mass, name)
         self.coullog = math.log(743*((1.5*k*T*6.242e+18)**1.5)/4.8e-8/math.sqrt(n)/Z)
-        print(self.coullog)
    
-    def gen_vel_vector(self):
+    def gen_vel_vector(self, n_total):
         sigma = math.sqrt(k*self.T/self.mass)
-        return np.random.randn(3,120) * sigma
+        return np.random.randn(3,n_total) * sigma
